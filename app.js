b05a31e4a51a0cf73714f13ae74c2664ae6b1a99
Nathalie Cazemajou <nat.caze@gmail.com>
2022-10-26 01:50:37 +0100

    solved

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+//creating a route
+app.get("/home", (req, res, next) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/about", (req, res, next) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/works", (req, res, next) => {
+  res.sendFile(__dirname + "/views/works.html");
+});
+
+app.get("/gallery", (req, res, next) => {
+  res.sendFile(__dirname + "/views/gallery.html");
+});
+
+app.listen(3000, () => console.log("App running on port 3000"));
